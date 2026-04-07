ac9fe1340858f215485e749a57087e72f2a63d17
dbourse29-cell <dbourse29@gmail.com>
2026-04-07 19:04:48 +0100

    Fix output comments in operator precedence exercise

--- 2. Python Basics/1-ExerciseOperatorPrecedence.py
+++ 2. Python Basics/1-ExerciseOperatorPrecedence.py
@@ -2,12 +2,12 @@
 # Try to write down your answer before and see how you do... keep it mind I made it a little tricky for you :)
 
 print((5 + 4) * 10 / 2)
-
+45
 print(((5 + 4) * 10) / 2)
-
+45
 print((5 + 4) * (10 / 2))
-
+45
 print(5 + (4 * 10) / 2)
-
+22.5
 print(5 + 4 * 10 // 2)
-
+25
